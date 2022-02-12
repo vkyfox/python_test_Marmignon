2989aeb75a0ac6171147b13beaabaa00a4c40f27
vicky <rigaut.theotime@gmail.com>
2022-02-12 14:12:06 +0100

    added context for why i did exam5 twice

--- test.py
+++ test.py
@@ -76,7 +76,9 @@ def exam5(list_to_sort=[12, 322, 14323, 8766544, 77777]):
     return sorted_list[-2]
 
 
-# Theoretically would have been more optimized, but less python-oriented alternative
+# Theoretically could have been more optimized, but less python-oriented alternative
+# Context: I was curious which method would be the fastest between native python methods
+# or the usually faster direct approach.
 def exam5_2(list_to_sort=[12, 322, 14323, 8766544, 77777]):
     """
     Finds the second-largest element in a given list. Will raise IndexError if insufficient number of elements.
@@ -128,7 +130,7 @@ if __name__ == '__main__':
     print(f'The second-largest element in the list "{_var_exam5}" is "{exam5(_var_exam5)}"')
     print(f'The second-largest element in the list "{_var_exam5_2}" is "{exam5_2(_var_exam5_2)}"')
 
-    # # Test efficiency of both methods for the 5th question
+    # # Efficiency test of both methods for the 5th question
     # import timeit
     # print("Time taken with python's sort method (executed 10000 times):")
     # print(timeit.timeit("exam5([i for i in range(5555)][::-1])", number=10000, globals={'exam5': exam5}))
